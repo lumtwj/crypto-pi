295ba5833864f37f3792e96b605654fa79388579
lumtwj <lumtwj@gmail.com>
2021-06-13 15:02:44 +0800

    Clear display when script is interrupted

--- main.py
+++ main.py
@@ -19,32 +19,35 @@ w, h = 250, 122
 font = ImageFont.truetype(os.path.join('./assets/OpenSans-Bold.ttf'), 14)
 
 crypto_iterator = CryptoIterator(data)
-while True:
-    data = chart.load()
-    chart_data = data[crypto_iterator.get()]
+try:
+    while True:
+        data = chart.load()
+        chart_data = data[crypto_iterator.get()]
 
-    symbol = chart_data['symbol'].upper()
-    chart_path = chart_data['chart_path']
-    price = chart_data['price']
-    percentage_change = chart_data['percentage']
+        symbol = chart_data['symbol'].upper()
+        chart_path = chart_data['chart_path']
+        price = chart_data['price']
+        percentage_change = chart_data['percentage']
 
-    print(symbol, price, percentage_change)
+        print(symbol, price, percentage_change)
 
-    image = Image.new(mode='1', size=(epd2in13_V2.EPD_HEIGHT, epd2in13_V2.EPD_WIDTH), color=255)
-    HRedImage = Image.new(mode='1', size=(epd2in13_V2.EPD_HEIGHT, epd2in13_V2.EPD_WIDTH), color=255)
-    draw = ImageDraw.Draw(image)
+        image = Image.new(mode='1', size=(epd2in13_V2.EPD_HEIGHT, epd2in13_V2.EPD_WIDTH), color=255)
+        HRedImage = Image.new(mode='1', size=(epd2in13_V2.EPD_HEIGHT, epd2in13_V2.EPD_WIDTH), color=255)
+        draw = ImageDraw.Draw(image)
 
-    # draw chart
-    chart_image = Image.open(chart_path)
-    image.paste(chart_image, (0, 0))
+        # draw chart
+        chart_image = Image.open(chart_path)
+        image.paste(chart_image, (0, 0))
 
-    # draw text
-    text = symbol + '  $' + price + '  ' + percentage_change
-    tw, th = font.getsize(text)
-    draw.text((10, h - th - 5), text, font=font, fill=0)
+        # draw text
+        text = symbol + '  $' + price + '  ' + percentage_change
+        tw, th = font.getsize(text)
+        draw.text((10, h - th - 5), text, font=font, fill=0)
 
-    rotated_image = image.rotate(180)
+        rotated_image = image.rotate(180)
 
-    epd.display(epd.getbuffer(rotated_image))
+        epd.display(epd.getbuffer(rotated_image))
 
-    crypto_iterator.next()
+        crypto_iterator.next()
+except KeyboardInterrupt:
+    epd.Clear(0xFF)
